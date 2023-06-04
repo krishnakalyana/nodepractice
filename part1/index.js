e778ec8a1217d86ec3d3e3105c6867f355972f6c
krishnakalyana <Krishna@sflhub.com>
2023-06-04 23:18:10 +0530

    updates

--- part1/index.js
+++ part1/index.js
@@ -2,15 +2,16 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const path = require("path")
 
-const adminRoutes = require("./routes/adminroutes")
+const adminData = require("./routes/adminroutes")
 const shopRoutes = require("./routes/shoproutes")
 const rootDir = require("./utils/path")
 
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use("/admin", adminRoutes)
+app.use("/admin", adminData.routes)
 app.use(shopRoutes)
+app.use(express.static(path.join(rootDir, "public")))
 
 app.use("/", (req, res, next) => {
     res.status(404).sendFile(path.join(rootDir, "views", "notfound.html"))
--- part1/routes/adminroutes.js
+++ part1/routes/adminroutes.js
@@ -3,13 +3,17 @@ const path = require('path')
 
 const rootDir = require("../utils/path")
 
+const products = []
+
 const router = express.Router()
 router.get("/add-product", (req, res) => {
     res.sendFile(path.join(rootDir, "views", "add-product.html"))
 })
 router.post('/add-product', (req, res) => {
     console.log(req.body);
-    res.send("<h1>Productpage</h1>")
+    products.push({ title: req.body.title })
+    res.redirect("/")
 })
 
-module.exports = router
+exports.routes = router
+exports.products = products
